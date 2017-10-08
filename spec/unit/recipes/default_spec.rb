1cae16ba33fcc513849a7d5a1e394eb63bb3681d
Marcel B <marcel@beestig.nl>
2017-10-08 20:32:21 +0200

    add windows support

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -22,7 +22,8 @@ describe 'telegraf::default' do
   platforms = {
     'centos' => ['6.6', '7.0'],
     'ubuntu' => ['14.04', '15.04'],
-    'amazon' => ['2015.09']
+    'amazon' => ['2015.09'],
+    'windows' => ['2012r2'],
   }
 
   platforms.each do |platform, versions|
